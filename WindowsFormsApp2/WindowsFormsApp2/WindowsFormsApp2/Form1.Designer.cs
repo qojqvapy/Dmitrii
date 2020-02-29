e8a112268c77d8c769cbbfdb651aac63b6ebd3d0
qojqvapy <61636295+qojqvapy@users.noreply.github.com>
2020-03-01 01:13:07 +0300

    All five problems, at last)

---
+++ WindowsFormsApp2/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rdfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.кругToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.четыреКругаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.четыреКругаToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.восемьКруговToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rdfToolStripMenuItem,
+            this.кругToolStripMenuItem,
+            this.четыреКругаToolStripMenuItem,
+            this.четыреКругаToolStripMenuItem1,
+            this.восемьКруговToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rdfToolStripMenuItem
+            // 
+            this.rdfToolStripMenuItem.Name = "rdfToolStripMenuItem";
+            this.rdfToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.rdfToolStripMenuItem.Text = "Квадрат";
+            this.rdfToolStripMenuItem.Click += new System.EventHandler(this.rdfToolStripMenuItem_Click);
+            // 
+            // кругToolStripMenuItem
+            // 
+            this.кругToolStripMenuItem.Name = "кругToolStripMenuItem";
+            this.кругToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.кругToolStripMenuItem.Text = "Круг";
+            this.кругToolStripMenuItem.Click += new System.EventHandler(this.кругToolStripMenuItem_Click);
+            // 
+            // четыреКругаToolStripMenuItem
+            // 
+            this.четыреКругаToolStripMenuItem.Name = "четыреКругаToolStripMenuItem";
+            this.четыреКругаToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.четыреКругаToolStripMenuItem.Text = "Много кругов";
+            this.четыреКругаToolStripMenuItem.Click += new System.EventHandler(this.четыреКругаToolStripMenuItem_Click);
+            // 
+            // четыреКругаToolStripMenuItem1
+            // 
+            this.четыреКругаToolStripMenuItem1.Name = "четыреКругаToolStripMenuItem1";
+            this.четыреКругаToolStripMenuItem1.Size = new System.Drawing.Size(90, 20);
+            this.четыреКругаToolStripMenuItem1.Text = "Четыре круга";
+            this.четыреКругаToolStripMenuItem1.Click += new System.EventHandler(this.четыреКругаToolStripMenuItem1_Click);
+            // 
+            // восемьКруговToolStripMenuItem
+            // 
+            this.восемьКруговToolStripMenuItem.Name = "восемьКруговToolStripMenuItem";
+            this.восемьКруговToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.восемьКруговToolStripMenuItem.Text = "Восемь кругов";
+            this.восемьКруговToolStripMenuItem.Click += new System.EventHandler(this.восемьКруговToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rdfToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem кругToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem четыреКругаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem четыреКругаToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem восемьКруговToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        class Figure
+        {
+            public float x2;
+            public float x1;
+            public float y2;
+            public float y1;
+            public Pen pen;
+
+            public Figure(Pen pen,float x1, float y1 , float x2 , float y2)
+            {
+                this.pen = pen;
+                this.x1 = x1;
+                this.x2 = x2;
+                this.y1 = y1;
+                this.y2 = y2;
+            }
+        }
+
+        private Figure[] figures = new Figure[10000];
+        int fg = 0;
+        int md = 0;
+        float r = 0;
+        float t = 0;
+        private PointF start;
+        private PointF res;
+        private float size;
+        private int mode = 0;
+        bool fp = true;
+        bool sp = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (md == 1)
+            {
+                if (mode != 0)
+                {
+                    Pen pen = new Pen(Color.Red);
+                    e.Graphics.DrawRectangle(pen, start.X, start.Y, size, size);
+                }
+            }
+            if (md == 2)
+            {
+                if (mode != 0)
+                {
+                    Pen pen = new Pen(Color.Magenta);
+                    r = (float)Math.Sqrt((res.X - start.X) * (res.X - start.X) + (res.Y - start.Y) * (res.Y - start.Y));
+                    t = (float)Math.Sqrt(r*r+r*r);
+                    e.Graphics.DrawEllipse(pen, start.X-r, start.Y-r, 2*r , 2*r);
+                }
+            }
+            if (md == 3)
+            {
+                if (mode != 0)
+                {
+                    Pen pen = new Pen(Color.Magenta);
+                    r = (float)Math.Sqrt((res.X - start.X) * (res.X - start.X) + (res.Y - start.Y) * (res.Y - start.Y));
+                    t = (float)Math.Sqrt(r * r + r * r);
+                    e.Graphics.DrawEllipse(pen, start.X - r, start.Y - r, 2 * r, 2 * r);
+                    for(int i = 0 ; i <= fg-1 ; i++)
+                    {
+                        e.Graphics.DrawEllipse(pen,figures[i].x1, figures[i].y1, figures[i].x2, figures[i].y2);
+                    }
+                }
+            }
+            if (md == 4)
+            {
+                if (mode != 0)
+                {
+                    Pen pen = new Pen(Color.Magenta);
+                    float rad = start.X - res.X;
+                    if (fp)
+                    {
+                        figures[fg] = new Figure(pen, start.X, start.Y, rad * 2 / 3, rad * 2 / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X - rad, start.Y, rad * 2 / 3, rad * 2 / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X - rad, start.Y - rad, rad * 2 / 3, rad * 2 / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X, start.Y - rad, rad * 2 / 3, rad * 2 / 3);
+                        fg++;
+                        fp = false;
+                    }
+                    else
+                    {
+                        figures[fg-4] = new Figure(pen, start.X, start.Y, rad * 2 / 3, rad * 2 / 3);
+                        figures[fg-3] = new Figure(pen, start.X - rad, start.Y, rad * 2 / 3, rad * 2 / 3);
+                        figures[fg-2] = new Figure(pen, start.X - rad, start.Y - rad, rad * 2 / 3, rad * 2 / 3);
+                        figures[fg-1] = new Figure(pen, start.X, start.Y - rad, rad * 2 / 3, rad * 2 / 3);
+                    }
+                    for (int i = 0; i <= fg - 1; i++)
+                    {
+                        e.Graphics.DrawEllipse(figures[i].pen, figures[i].x1, figures[i].y1, figures[i].x2, figures[i].y2);
+                    }
+                }
+            }
+            if (md == 5)
+            {
+                if (mode != 0)
+                {
+                    Pen pen = new Pen(Color.Magenta);
+                    float rad = start.X - res.X;
+                    if (fp)
+                    {
+                        figures[fg] = new Figure(pen, start.X, start.Y + rad, rad / 3, rad / 3 );
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X, start.Y - rad, rad / 3, rad / 3);
+                        fg++; 
+                        figures[fg] = new Figure(pen, start.X + rad, start.Y, rad / 3, rad / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X - rad, start.Y, rad / 3, rad / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X + rad / (float)Math.Sqrt(2), start.Y + rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X - rad / (float)Math.Sqrt(2), start.Y - rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X + rad / (float)Math.Sqrt(2), start.Y - rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                        fg++;
+                        figures[fg] = new Figure(pen, start.X - rad / (float)Math.Sqrt(2), start.Y + rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                        fg++;
+                        fp = false;
+                    }
+                    else
+                    {
+                        figures[fg - 8] = new Figure(pen, start.X, start.Y + rad, rad / 3, rad / 3);
+                        figures[fg - 7] = new Figure(pen, start.X, start.Y - rad, rad / 3, rad / 3);
+                        figures[fg - 6] = new Figure(pen, start.X + rad, start.Y, rad / 3, rad / 3);
+                        figures[fg - 5] = new Figure(pen, start.X - rad, start.Y, rad / 3, rad / 3);
+                        figures[fg - 4] = new Figure(pen, start.X + rad / (float)Math.Sqrt(2), start.Y + rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                        figures[fg - 3] = new Figure(pen, start.X - rad / (float)Math.Sqrt(2), start.Y - rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                        figures[fg - 2] = new Figure(pen, start.X + rad / (float)Math.Sqrt(2), start.Y - rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                        figures[fg - 1] = new Figure(pen, start.X - rad / (float)Math.Sqrt(2), start.Y + rad / (float)Math.Sqrt(2), rad / 3, rad / 3);
+                    }
+                    for (int i = 0; i <= fg - 1; i++)
+                    {
+                        e.Graphics.DrawEllipse(figures[i].pen, figures[i].x1, figures[i].y1, figures[i].x2, figures[i].y2);
+                    }
+                }
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start = new PointF(e.X, e.Y);
+            fp = true;
+            mode = 1;
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            size = Math.Min(Math.Abs(start.X - e.X), Math.Abs(start.Y - e.Y));
+            mode = 2;
+            res = new PointF(e.X, e.Y);
+            Refresh();
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mode == 1)
+            {
+                size = Math.Min(Math.Abs(start.X - e.X),
+                Math.Abs(start.Y - e.Y));
+                res = new PointF(e.X, e.Y);
+                Refresh();
+            }
+        }
+        private void rdfToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            md = 1;
+        }
+        private void кругToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            md = 2;
+        }
+        private void четыреКругаToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            md = 4;
+        }
+        private void четыреКругаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            md = 3;
+        }
+        private void восемьКруговToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            md = 5;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
